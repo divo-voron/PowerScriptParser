b4a376c982d4a4065331c34c76064b03a7a5bcd8
Dzmitry Voranau <dzmitry_voranau@epam.com>
2020-06-01 15:52:00 +0300

    Initial commit

---
+++ PowerScriptParser/Helper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace PowerScriptParser
+{
+    public static class Helper
+    {
+        public static List<int> GetLineNumbers(string[] lines, string text)
+        {
+            return GetLineNumbers(lines, text, _ => true);
+        }
+
+        public static List<int> GetLineNumbers(string[] lines, string text, Func<string, bool> condition)
+        {
+            var lineNumbers = new List<int>();
+            for (var index = 0; index < lines.Length; index++)
+            {
+                var line = lines[index];
+                if (Regex.IsMatch(line, text, RegexOptions.IgnoreCase) && condition(line))
+                {
+                    lineNumbers.Add(index);
+                }
+            }
+
+            return lineNumbers;
+        }
+
+        public static bool TryGetStartNum(int number, string[] lines, out int startNumber)
+        {
+            startNumber = 0;
+            for (var i = number; i > 0; i--)
+            {
+                var line = lines[i];
+                if (!Regex.IsMatch(line, @"^(public|private)\s+function\s") &&
+                    !Regex.IsMatch(line, @"^(public|private)\s+subroutine\s") &&
+                    !Regex.IsMatch(line, @"^event\s+"))
+                {
+                    continue;
+                }
+
+                startNumber = i;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TryGetEndNum(int number, string[] lines, out int endNumber)
+        {
+            endNumber = 0;
+            for (var i = number; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                if (!Regex.IsMatch(line, @"^end\s+function") &&
+                    !Regex.IsMatch(line, @"^end\s+subroutine") &&
+                    !Regex.IsMatch(line, @"^end\s+event"))
+                {
+                    continue;
+                }
+
+                endNumber = i;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ PowerScriptParser/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PowerScriptParser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullPath = @"d:\MyUtilities\PowerScriptParser\_Sources\w_cladmin_subscriber\w_cladmin_subscriber.srw";
+
+            var lines = File.ReadAllLines(fullPath);
+
+            var path = Path.GetDirectoryName(fullPath);
+
+            GetSubMethods(lines, path, "su_alternate_subs_id");
+
+            //FindAllEntrances(lines, @"cbd_renewal_mnth");
+
+            Console.ReadKey();
+        }
+
+        private static void GetSubMethods(string[] lines, string path, string text)
+        {
+            var lineNumbers = Helper.GetLineNumbers(lines, text);
+
+            var numbers = new List<Tuple<int, int>>();
+
+            foreach (var number in lineNumbers)
+            {
+                if (!Helper.TryGetStartNum(number, lines, out var startNumber) ||
+                    !Helper.TryGetEndNum(number, lines, out var endNumber))
+                {
+                    continue;
+                }
+
+                var value = new Tuple<int, int>(startNumber, endNumber);
+                if (numbers.Contains(value))
+                {
+                    continue;
+                }
+
+                numbers.Add(value);
+                var subLines = lines.Skip(startNumber).Take(endNumber - startNumber + 1);
+
+                var directoryPath = $@"{path}\methods";
+                Directory.CreateDirectory(directoryPath);
+
+                var filePath = $@"{path}\methods\{number}.srw";
+                File.WriteAllLines(filePath, subLines);
+
+                Console.WriteLine($"Created new file: {filePath}");
+            }
+
+            Console.WriteLine("End.");
+        }
+
+
+
+        private static void FindAllEntrances(string[] lines, string text)
+        {
+            var lineNumbers = Helper.GetLineNumbers(lines, text, line => !line.Trim().StartsWith("/"));
+
+            foreach (var number in lineNumbers)
+            {
+                Console.WriteLine($"{number + 1} : {lines.ElementAt(number).Trim()}");
+            }
+        }
+    }
+}
